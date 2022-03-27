e7eee7839865f57d76fdb847f0a5bb74210f060e
nabu0305 <nabu010305@gmail.com>
2022-03-27 05:42:30 +0000

    Chat completed

--- src/server.js
+++ src/server.js
@@ -3,26 +3,26 @@ import http from "http";
 import WebSocket from "ws";
 const app = express();
 
-app.set("view engine","pug");
-app.set("views",__dirname+ "/views");
-app.use("/public",express.static(__dirname+"/public"));
-app.get("/",(req,res)=>res.render("home"));
-app.get("/*",(req,res)=>res.redirect("/"));
-const handleListen = () => console.log(`Listening on https://zoomcloning-lwcbl.run.goorm.io/`);
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+const handleListen = () =>
+  console.log(`Listening on https://zoomcloning-lwcbl.run.goorm.io/`);
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
+const wss = new WebSocket.Server({ server });
 
+const sockets = [];
 
-
-wss.on("connection",(socket) => {
-    console.log("Connected to Browser ✔");
-    socket.on("close",()=> console.log("DisConnection to Browser ❌"));
-    socket.on("message",(message)=>{
-        console.log(message.toString('utf8'));
-    });
-    socket.send("hello!!!");
-
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  console.log("Connected to Browser ✔");
+  socket.on("close", () => console.log("DisConnection to Browser ❌"));
+  socket.on("message", (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message));
+  });
 });
 
-server.listen(3000,handleListen);
+server.listen(3000, handleListen);
