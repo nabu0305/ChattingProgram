7cab17b4dd992863b7fbd258bb7675ef37e9c4eb
nabu0305 <nabu010305@gmail.com>
2022-03-27 02:25:27 +0000

    WebSocket Messages

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open",()=>{
+    console.log("Connection to Server ✔");
+});
+
+socket.addEventListener("message",(message) =>{
+    console.log("New massage: ",message.data);
+});
+
+socket.addEventListener("close",()=>{
+    console.log("DisConnection to Server ❌");
+});
+
+setTimeout(() => {
+    socket.send("Hello from the browser");
+}, 10000);
