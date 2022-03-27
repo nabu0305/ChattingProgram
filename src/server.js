f7000df82005e6e10316ec4fe1161b43a3d515e9
nabu0305 <nabu010305@gmail.com>
2022-03-27 01:24:20 +0000

    WebSocket in nodejs

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 import express from "express";
-
+import http from "http";
+import WebSocket from "ws";
 const app = express();
 
 app.set("view engine","pug");
 app.set("views",__dirname+ "/views");
 app.use("/public",express.static(__dirname+"/public"));
 app.get("/",(req,res)=>res.render("home"));
-
+app.get("/*",(req,res)=>res.redirect("/"));
 const handleListen = () => console.log(`Listening on https://zoomcloning-lwcbl.run.goorm.io/`);
-app.listen(3000,handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+server.listen(3000,handleListen);
