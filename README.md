23890bf635e6c6601e748d92dbd322fb24a0a503
nabu0305 <nabu010305@gmail.com>
2022-03-27 10:19:05 +0000

    Instaling SocketIO

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ZoomCloning
 
 ZoomClone using NodeJS and WebSockets.
+
+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
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 const app = express();
 
 app.set("view engine", "pug");
@@ -8,14 +8,12 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
-const handleListen = () =>
-  console.log(`Listening on https://zoomcloning-lwcbl.run.goorm.io/`);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-const sockets = [];
-
+wsServer.on("connection", (socket) => {});
+/*
 wss.on("connection", (socket) => {
   sockets.push(socket);
   socket["nickname"] = "Anon";
@@ -33,5 +31,8 @@ wss.on("connection", (socket) => {
     }
   });
 });
+*/
 
-server.listen(3000, handleListen);
+const handleListen = () =>
+  console.log(`Listening on https://zoomcloning-lwcbl.run.goorm.io/`);
+httpServer.listen(3000, handleListen);
